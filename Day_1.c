bd70c40e9ebf74b348d7213365b3177547780883
Namish320 <Namish.38318@stu.upes.ac.in>
2026-09-05 07:53:03 +0530

    Implement sum calculation for two numbers
    Fix input and output for summation program.

---
+++ Day_1.c
@@ -0,0 +1,12 @@
+Q1: Write a program to input two numbers and display their sum.
+
+#include <stdio.h>
+int main(){
+int sum;
+int a,b;
+  printf("Enter the numbers:\n");
+scanf("%d",&a ,&b);
+sum = a+b;
+printf("the sum is :%d\n");
+return 0;
+}
